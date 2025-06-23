65a149b39fc7fdbd1fd9d5f963ace10c21d88003
samuelitwaru <samuelitwaru@gmail.com>
2025-06-23 06:43:48 +0300

    more

---
+++ CKEditor/myconfig.js
@@ -0,0 +1,8 @@
+CKEDITOR.config.toolbar_myToolbar = [
+  ["Bold", "Italic", "Underline"],
+  ["NumberedList", "BulletedList"],
+  ["Link"],
+];
+
+CKEDITOR.config.forcePasteAsPlainText = true;
+// CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
