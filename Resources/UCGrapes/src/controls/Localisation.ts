867b8447b91d877b49f37f0117c8cf151fa4550a
samuelitwaru <samuelitwaru@gmail.com>
2025-07-21 10:24:35 +0300

    more

--- Resources/UCGrapes/src/controls/Localisation.ts
+++
@@ -1,8 +0,0 @@
-export class Localisation {
-  async loadTranslations(i18n: any, locale: any) {
-    const response = await fetch(`../i18n/${locale}.json`);
-    const translations = await response.json();
-
-    i18n.store(translations);
-  }
-}
--- Resources/UCGrapes/src/utils/env.ts
+++
@@ -1,1 +0,0 @@
-export const environment = '/Comforta_version2DevelopmentNETPostgreSQL';
